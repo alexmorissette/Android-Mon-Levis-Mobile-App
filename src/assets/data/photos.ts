fc70876128907ac4019a2359830ae377f81827aa
alexmorissette <36205873+alexmorissette@users.noreply.github.com>
2021-09-30 23:27:04 -0400

    Commit Gallerie photos

---
+++ src/assets/data/photos.ts
@@ -0,0 +1,32 @@
+import {Photo} from "src/app/interfaces/photo";
+
+export const PHOTOS: Photo[] = [
+{
+    id: 1,
+    titre: "Vieux Lévis",
+    img: "batiments.jpg"
+},
+{
+    id: 2,
+    titre: "Lévis en hiver",
+    img: "hiver.jpg"
+},
+{
+    id: 3,
+    titre: "Quai Paquet",
+    img: "paquet.jpg"
+
+},
+{
+    id: 4,
+    titre: "Parc",
+    img: "parc.jpg"
+
+},
+{
+    id: 5,
+    titre: "Vue sur le fleuve",
+    img: "vuefleuve.jpg"
+
+}
+]
